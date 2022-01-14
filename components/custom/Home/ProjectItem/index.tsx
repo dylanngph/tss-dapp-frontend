6165167f672d31c665b1e3e50e3f9bf26dea21b2
Thap Nguyen <tomhalan@jadelabs.org>
2022-01-14 17:06:54 +0700

    style image in table list

--- components/custom/Home/ProjectItem/index.tsx
+++ components/custom/Home/ProjectItem/index.tsx
@@ -25,8 +25,8 @@ export default function ProjectItem ({project, index}: ProjectItemProps) {
     <tr onClick={() => { router.push(`/project/${project._id}`) }}>
       <td>{index + 1}</td>
       <td>
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Box sx={{ width: 34, height: 24, marginRight: '5px', '& img': {maxWidth: '100%', height: 'auto'} }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ width: 24, height: 24, marginRight: '5px', display: 'flex', justifyContent: 'center', alignItems: 'center', '& img': {maxWidth: '100%', height: 'auto'} }}>
             <img src={project?.logo} alt={project.projectName}/>
           </Box>
           <span>{project.projectName}</span>
