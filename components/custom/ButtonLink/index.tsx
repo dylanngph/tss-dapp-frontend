89ce2fba9f527b9e8eb6d1728a4c3e6e5c9014ff
Quang Huy <quanhuyc12ltqtm@gmail.com>
2022-01-26 18:23:44 +0700

    fix bug for build code

--- components/custom/ButtonLink/index.tsx
+++ components/custom/ButtonLink/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export interface ButtonLinkProps {
   title: string,
-  href: string,
+  href: any,
 }
 
 export default function ButtonLink ({title, href}: ButtonLinkProps) {
