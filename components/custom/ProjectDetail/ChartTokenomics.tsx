d8973490c2db5c3eac37dfc4fc471617a3b0740a
Quang Huy <quanhuyc12ltqtm@gmail.com>
2022-01-27 14:57:08 +0700

    fix don't show dinamic in tokennomic

--- components/custom/ProjectDetail/ChartTokenomics.tsx
+++ components/custom/ProjectDetail/ChartTokenomics.tsx
@@ -124,7 +124,7 @@ export default function ChartTokenomics ({tokenAllocations, dataChart}: ChartTok
                     >
                       <TableCell component="th" scope="row">{index + 1}</TableCell>
                       <TableCell>{row.allocationName}</TableCell>
-                      <TableCell align="right">{(row.rate).toFixed()}&#37;</TableCell>
+                      <TableCell align="right">{(row.rate)}&#37;</TableCell>
                       <TableCell align="right">{new Intl.NumberFormat('de-DE').format(row.price)}</TableCell>
                       <TableCell align="right">{new Intl.NumberFormat('de-DE').format(row.amount)}</TableCell>
                       <TableCell>{row.vesting}</TableCell>
